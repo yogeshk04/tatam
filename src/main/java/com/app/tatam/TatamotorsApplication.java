5fa16c4f5a9a5cff6a035b7504f5d4836f5a9ffe
Yogesh Nikam <36443045+yogeshk04@users.noreply.github.com>
2021-04-18 07:38:01 +0530

    Initial commit

---
+++ src/main/java/com/app/tatam/TatamotorsApplication.java
@@ -0,0 +1,13 @@
+package com.app.tatam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TatamotorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TatamotorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/tatam/TatamotorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.tatam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TatamotorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
